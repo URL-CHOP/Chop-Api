e503e6553efac9efdc61a2ab114a1336d44b10d5
pjh374968@gmail.com <pjh374968@gmail.com>
2019-01-09 09:22:33 +0900

    [refactor] #11 H2 삭제 및 Mysql 연동
    h2 의존성 삭제 및 mysql 의존성 추가
    yml 설정 추가
    DataLoader 삭제

--- src/main/java/me/nexters/chop/bootstrap/DataLoader.java
+++ src/main/java/me/nexters/chop/bootstrap/DataLoader.java
@@ -4,12 +4,10 @@ import me.nexters.chop.dto.url.UrlSaveRequestDto;
 import me.nexters.chop.repository.ShortenRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
 
 /**
  * @author junho.park
  */
-@Component
 public class DataLoader implements ApplicationRunner {
 
     private final ShortenRepository shortenRepository;
@@ -21,8 +19,8 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         UrlSaveRequestDto sampleDto = UrlSaveRequestDto.builder()
-                .longUrl("looonggg")
-                .shortUrl("aa")
+                .longUrl("https://namu.wiki/w/%EC%B9%98%ED%82%A8")
+                .shortUrl("a")
                 .build();
 
         shortenRepository.save(sampleDto.toEntity());
