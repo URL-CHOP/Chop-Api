ebdae91bd3787aac820b6417f9fc8d285c3ab567
pjh374968@gmail.com <pjh374968@gmail.com>
2019-01-20 00:13:23 +0900

    [refactor] #29 Redirect 서비스 테스트 리팩토링
    mock test로 변경

--- src/test/java/me/nexters/chop/service/RedirectServiceTest.java
+++ src/test/java/me/nexters/chop/service/RedirectServiceTest.java
@@ -2,42 +2,64 @@ package me.nexters.chop.service;
 
 import me.nexters.chop.domain.url.Url;
 import me.nexters.chop.repository.RedirectRepository;
-import me.nexters.chop.repository.ShortenRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.persistence.EntityNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author junho.park
  */
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class RedirectServiceTest {
-
-    @Autowired
+    @Mock
     private RedirectRepository redirectRepository;
 
-    private String shortenUrl;
-    private String expectedOriginUrl;
+    @InjectMocks
+    private RedirectService redirectService;
 
-    @BeforeEach
-    public void setUp() {
-        shortenUrl = "a";
-        expectedOriginUrl = "https://namu.wiki/w/%EC%B9%98%ED%82%A8";
+    @Value("${string.originUrl}")
+    private String originUrl;
+
+    @Value("${string.shortUrl}")
+    private String shortUrl;
+
+    @Test
+    public void redirect_IsSuccess() {
+        Url url = Url.builder()
+                .shortUrl(shortUrl)
+                .originUrl(originUrl)
+                .build();
+
+        given(redirectRepository.findUrlByShortUrl(shortUrl)).willReturn(url);
+
+        Url returnedUrl = redirectService.redirect(shortUrl);
+
+        assertThat(returnedUrl.getOriginUrl()).isEqualTo(originUrl);
+        assertThat(returnedUrl.getShortUrl()).isEqualTo(shortUrl);
+        verify(redirectRepository, times(1)).findUrlByShortUrl(shortUrl);
     }
 
     @Test
-    @Transactional
-    public void findUrlByShortUrl_isSuccess() {
-        Url returnedUrl = redirectRepository.findUrlByShortUrl(shortenUrl);
+    public void redirect_isFailedDueToNotExistingShortUrl_throwException() {
+        String notExistingUrl = "notExists";
+        given(redirectRepository.findUrlByShortUrl(notExistingUrl)).willReturn(null);
 
-        assertEquals(expectedOriginUrl, returnedUrl.getOriginUrl());
+        assertThrows(EntityNotFoundException.class, () -> redirectService.redirect(notExistingUrl));
     }
 }
