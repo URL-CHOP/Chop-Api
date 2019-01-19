3abc648736917b8ca3acb6a5893278fc2786a3f7
pjh374968@gmail.com <pjh374968@gmail.com>
2019-01-20 00:14:07 +0900

    [refactor] #29 Shorten 서비스 테스트 리팩토링
    mock test로 변경

--- src/test/java/me/nexters/chop/service/ShortenServiceTest.java
+++ src/test/java/me/nexters/chop/service/ShortenServiceTest.java
@@ -1,36 +1,40 @@
 package me.nexters.chop.service;
 
 import me.nexters.chop.domain.url.Url;
+import me.nexters.chop.dto.url.UrlRequestDto;
 import me.nexters.chop.repository.ShortenRepository;
 import me.nexters.chop.repository.StatisticsRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author junho.park
  */
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@Transactional
 @SpringBootTest
 public class ShortenServiceTest {
+    @Mock
+    private StatisticsRepository statisticsRepository;
 
-    @Autowired
+    @Mock
     private ShortenRepository shortenRepository;
 
-    @Autowired
-    private StatisticsRepository statisticsRepository;
+    @InjectMocks
+    private ShortenService shortenService;
 
     @Value("${string.base62matchPattern}")
     private String base62matchPattern;
@@ -47,34 +51,48 @@ public class ShortenServiceTest {
     }
 
     @Test
-    @Transactional
-    public void save_SaveSuccess() {
-
+    public void save_success() {
         Url url = Url.builder()
                 .originUrl(originUrl)
                 .shortUrl(shortUrl)
                 .build();
 
-        Url responseUrl = shortenRepository.save(url);
+        given(shortenRepository.save(url)).willReturn(url);
+        given(shortenRepository.findUrlByOriginUrl(originUrl)).willReturn(url);
 
-        assertEquals(url.getOriginUrl(), originUrl);
-        assertEquals(responseUrl.getOriginUrl(), originUrl);
-    }
-
-    @Test
-    public void save_EmptyOriginUrl_DataIntegrityViolationException() {
-        Url url = Url.builder()
+        UrlRequestDto dto = UrlRequestDto.builder()
+                .originUrl(originUrl)
                 .build();
 
-        assertThrows(DataIntegrityViolationException.class, () ->
-            shortenRepository.save(url)
-        );
+        Url returnedUrl = shortenService.save(dto);
+
+        assertThat(returnedUrl.getOriginUrl()).isEqualTo(originUrl);
+        verify(shortenRepository, times(1)).getMaxId();
+        verify(shortenRepository, times(1)).findUrlByOriginUrl(originUrl);
     }
 
     @Test
-    public void urlCountPlus() {
-        int count = statisticsRepository.findByOriginUrl("https://namu.wiki/w/%EC%B9%98%ED%82%A8");
-        statisticsRepository.updateTotalCount("https://namu.wiki/w/%EC%B9%98%ED%82%A8");
-        assertEquals(count+1 , statisticsRepository.findByOriginUrl("https://namu.wiki/w/%EC%B9%98%ED%82%A8"));
+    public void saveEmptyUrl_isFailed_ThrowException() {
+        UrlRequestDto dto = UrlRequestDto.builder()
+                .build();
+
+        assertThrows(NullPointerException.class, () -> shortenService.save(dto));
+    }
+
+    @Test
+    public void totalCountPlus_IsSuccess() {
+        shortenService.totalCountPlus(originUrl);
+
+        verify(statisticsRepository, times(1)).updateTotalCount(originUrl);
+    }
+
+    @Test
+    void findMaxIdFromDatabase_isSuccess() {
+        given(shortenRepository.getMaxId()).willReturn(1L);
+
+        long maxId = shortenService.findMaxIdFromDatabase();
+
+        assertThat(maxId).isEqualTo(2);
+        verify(shortenRepository, times(1)).getMaxId();
     }
 }
