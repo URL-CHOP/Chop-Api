63e54dae24a7956c466a44ed40885d00289f32f1
manki.kim <manki.kim@navercorp.com>
2019-01-20 16:59:38 +0900

    Optional 이용하여 null처리 로직 수정

--- src/main/java/me/nexters/chop/service/RedirectService.java
+++ src/main/java/me/nexters/chop/service/RedirectService.java
@@ -1,29 +1,27 @@
 package me.nexters.chop.service;
 
-import me.nexters.chop.domain.url.Url;
-import me.nexters.chop.repository.RedirectRepository;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import me.nexters.chop.domain.url.Url;
+import me.nexters.chop.repository.RedirectRepository;
 
 /**
  * @author junho.park
  */
 @Service
 public class RedirectService {
-    private final RedirectRepository redirectRepository;
+	private final RedirectRepository redirectRepository;
 
-    public RedirectService(RedirectRepository redirectRepository) {
-        this.redirectRepository = redirectRepository;
-    }
+	public RedirectService(RedirectRepository redirectRepository) {
+		this.redirectRepository = redirectRepository;
+	}
 
-    public Url redirect(String shortenUrl) {
-        Url url = redirectRepository.findUrlByShortUrl(shortenUrl);
-
-        if (url == null) {
-            throw new EntityNotFoundException("nexters.me/" + shortenUrl+ " 는 등록되지 않은 URL 입니다.");
-        }
-
-        return url;
-    }
+	public Url redirect(String shortenUrl) {
+		return Optional.ofNullable(redirectRepository.findUrlByShortUrl(shortenUrl))
+			.orElseThrow(() -> new EntityNotFoundException("nexters.me/" + shortenUrl + " 는 등록되지 않은 URL 입니다."));
+	}
 }
