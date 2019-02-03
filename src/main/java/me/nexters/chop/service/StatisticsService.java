ce04b119c42f8292bb9b41c8557060c39ac7a33c
choitaehoon <xognsdl1230@naver.com>
2019-02-03 11:40:49 +0900

    statistics 예전 os 지우기

--- src/main/java/me/nexters/chop/service/StatisticsService.java
+++ src/main/java/me/nexters/chop/service/StatisticsService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import me.nexters.chop.repository.StatisticsRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author taehoon.choi 2019-01-11
@@ -22,25 +23,13 @@ import me.nexters.chop.repository.StatisticsRepository;
 public class StatisticsService {
 
     private final StatisticsRepository statisticsRepository;
-
-    public List<String> statisticsInsert(String shortUrl, String host, String userAgent) {
-
-        List<String> statisticsInformation = new ArrayList<>();
-        statisticsInformation.add(String.valueOf( statisticsRepository.findByShortUrl(shortUrl)) );
-        statisticsInformation.add(host);
-
-        Pattern pattern = Pattern.compile("(?<os>\\([a-zA-Z]*)");
-        Matcher matcher = pattern.matcher(userAgent);
-        if (matcher.find())
-            statisticsInformation.add(matcher.group("os").replace("(",""));
-
-        return statisticsInformation;
-    }
+    
 
     public long getGlobalCount() {
         return statisticsRepository.count();
     }
 
+    @Transactional
     public void totalCountPlus(String longUrl) {
         statisticsRepository.updateTotalCount(longUrl);
     }
