fea00ecedbc8981cf8eb1298c73842e3128d8514
manki.kim <manki.kim@navercorp.com>
2019-02-05 14:46:03 +0900

    #57 불필요한 코드제거 및 코드 정리

--- src/main/java/me/nexters/chop/api/RedirectController.java
+++ src/main/java/me/nexters/chop/api/RedirectController.java
@@ -2,23 +2,20 @@ package me.nexters.chop.api;
 
 import java.net.URI;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import me.nexters.chop.service.ShortenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import me.nexters.chop.domain.url.Url;
 import me.nexters.chop.dto.url.UrlResponseDto;
 import me.nexters.chop.service.RedirectService;
-import me.nexters.chop.service.StatisticsService;
 
 /**
  * @author junho.park
@@ -29,13 +26,10 @@ import me.nexters.chop.service.StatisticsService;
 public class RedirectController {
 
     private final RedirectService redirectService;
-    private final StatisticsService statisticsService;
-    private final ShortenService shortenService;
 
     @GetMapping("/{shortenUrl}")
     @ApiOperation(value = "Url 리다이렉트", notes = "단축 Url을 리다이렉트 해준다", response = UrlResponseDto.class)
-    public ResponseEntity<UrlResponseDto> redirect(@PathVariable("shortenUrl") String shortenUrl
-                                                 , @RequestHeader(value = "Referer",required = false) String referer){
+    public ResponseEntity<UrlResponseDto> redirect(@PathVariable("shortenUrl") String shortenUrl){
         Url url = redirectService.redirect(shortenUrl);
 
         String originUrl = url.getOriginUrl();
@@ -44,9 +38,6 @@ public class RedirectController {
                 .originUrl(originUrl)
                 .shortUrl(shortenUrl)
                 .build();
-
-        shortenService.statisticsInsert(referer,shortenUrl);
-        statisticsService.totalCountPlus(originUrl);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(originUrl));
