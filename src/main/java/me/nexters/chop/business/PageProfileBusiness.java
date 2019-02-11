6f6b3bf2d86af3aeaa26e961579d9fcc5840e888
choitaehoon <xognsdl1230@naver.com>
2019-02-12 01:16:15 +0900

    restTemplate title 디코딩 작업 완료

--- src/main/java/me/nexters/chop/business/PageProfileBusiness.java
+++ src/main/java/me/nexters/chop/business/PageProfileBusiness.java
@@ -9,8 +9,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * @author manki.kim
@@ -26,12 +29,26 @@ public class PageProfileBusiness {
 	private static final Pattern TITLE_REGEX = Pattern.compile("(<title>)([A-Z0-9a-zㄱ-ㅎㅏ-ㅣ가-힣:+&@#/%?=~_|!:,.;-]+)");
 
 	public String getTitle(String url) {
+
 		Matcher matcher = TITLE_REGEX.matcher(restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(new HttpHeaders()),String.class).getBody().replaceAll("\\s",""));
 		if (matcher.find()) {
-			return matcher.group(TITLE_GET);
+			 return Stream.of(matcher.group(TITLE_GET))
+					.map(title -> decoding(title))
+					 .findFirst()
+					 .get();
 		}
 
 		return null;
+	}
+
+
+	private String decoding(String s) {
+		try {
+			return URLDecoder.decode(s,"UTF-8");
+		}catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return s;
 	}
 
 }
