450cfe1b71e5e92a5c0ac1cee51b624a35d16b5f
manki.kim <manki.kim@navercorp.com>
2019-02-17 17:30:07 +0900

    #103 refacotring code

--- src/main/java/me/nexters/chop/api/StatisticsController.java
+++ src/main/java/me/nexters/chop/api/StatisticsController.java
@@ -1,15 +1,8 @@
 package me.nexters.chop.api;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import me.nexters.chop.api.grpc.ChopGrpcClient;
-import me.nexters.chop.config.time.TimeUtil;
-import me.nexters.chop.dto.stats.*;
-import me.nexters.chop.grpc.ClickCount;
-import me.nexters.chop.grpc.Platform;
-import me.nexters.chop.grpc.Referer;
-import me.nexters.chop.grpc.TotalCount;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import me.nexters.chop.api.grpc.ChopGrpcClient;
+import me.nexters.chop.config.time.TimeUtil;
+import me.nexters.chop.dto.stats.StatsClickResponseDto;
+import me.nexters.chop.dto.stats.StatsMainResponseDto;
+import me.nexters.chop.dto.stats.StatsPlatformResponseDto;
+import me.nexters.chop.dto.stats.StatsRefererResponseDto;
+import me.nexters.chop.dto.stats.StatsTotalResponseDto;
+import me.nexters.chop.grpc.ClickCount;
+import me.nexters.chop.grpc.Platform;
+import me.nexters.chop.grpc.Referer;
+import me.nexters.chop.grpc.TotalCount;
 
 /**
  * @author junho.park
@@ -30,7 +35,7 @@ import java.util.List;
 public class StatisticsController {
 	private final ChopGrpcClient grpcClient;
 
-	@GetMapping("/chop/v1/platform/{shortenUrl}")
+	@GetMapping("/api/v1/urls/{shortenUrl}/platform}")
 	@ApiOperation(value = "해당 url platform 반환", notes = "해당 url의 mobile, browser 카운트를 반환한다.", response = StatsMainResponseDto.class)
 	public ResponseEntity<StatsPlatformResponseDto> platformRequest(@PathVariable("shortenUrl") String shortenUrl) {
 		Platform platform = grpcClient.getPlatformStats(shortenUrl);
@@ -43,25 +48,22 @@ public class StatisticsController {
 		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 
-	@GetMapping("/chop/v1/referer/{shortenUrl}")
+	@GetMapping("/api/v1/urls/{shortenUrl}/referer")
 	@ApiOperation(value = "해당 url referer 반환", notes = "해당 url의 referer 카운트를 반환한다.", response = StatsMainResponseDto.class)
 	public ResponseEntity<List<StatsRefererResponseDto>> refererRequest(@PathVariable("shortenUrl") String shortenUrl) {
 		List<Referer> referers = grpcClient.getRefererStats(shortenUrl);
 
-		List<StatsRefererResponseDto> dtoList = new ArrayList<>();
+		List<StatsRefererResponseDto> statsRefererResponse = referers.stream()
+			.map(referer -> StatsRefererResponseDto.builder()
+				.referer(referer.getReferer())
+				.count(referer.getCount())
+				.build())
+			.collect(Collectors.toList());
 
-		for (Referer referer : referers) {
-			StatsRefererResponseDto dto = StatsRefererResponseDto.builder()
-					.referer(referer.getReferer())
-					.count(referer.getCount())
-					.build();
-
-			dtoList.add(dto);
-		}
-		return new ResponseEntity<>(dtoList, HttpStatus.OK);
+		return new ResponseEntity<>(statsRefererResponse, HttpStatus.OK);
 	}
 
-	@GetMapping("/chop/v1/totalcount/{shortenUrl}")
+	@GetMapping("/api/v1/urls/{shortenUrl}/totalcount}")
 	@ApiOperation(value = "해당 url 총 클릭수 반환", notes = "해당 url의 총 클릭수를 반환한다.", response = StatsMainResponseDto.class)
 	public ResponseEntity<StatsTotalResponseDto> totalCountRequest(@PathVariable("shortenUrl") String shortenUrl) {
 		TotalCount totalCount = grpcClient.getTotalCount(shortenUrl);
@@ -73,23 +75,19 @@ public class StatisticsController {
 		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 
-	@GetMapping("/chop/v1/clickdate/{shortenUrl}")
+	@GetMapping("/api/v1/urls/{shortenUrl}/clickdate")
 	@ApiOperation(value = "해당 url 총 클릭수 반환", notes = "해당 url의 총 클릭수를 반환한다.", response = StatsMainResponseDto.class)
 	public ResponseEntity<List<StatsClickResponseDto>> clickCountRequest
 			(@PathVariable("shortenUrl") String shortenUrl, @RequestParam int week) {
-		List<StatsClickResponseDto> dtoList = new ArrayList<>();
 
 		List<ClickCount> clickCounts = grpcClient.getClickCount(shortenUrl, week);
+		List<StatsClickResponseDto> response = clickCounts.stream()
+			.map(clickCount -> StatsClickResponseDto.builder()
+				.clickDate(TimeUtil.convertTimestampToString(clickCount.getDate()))
+				.count(clickCount.getCount())
+				.build())
+			.collect(Collectors.toList());
 
-		for (ClickCount clickCount : clickCounts) {
-			StatsClickResponseDto dto = StatsClickResponseDto.builder()
-					.clickDate(TimeUtil.convertTimestampToString(clickCount.getDate()))
-					.count(clickCount.getCount())
-					.build();
-
-			dtoList.add(dto);
-		}
-
-		return new ResponseEntity<>(dtoList, HttpStatus.OK);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
