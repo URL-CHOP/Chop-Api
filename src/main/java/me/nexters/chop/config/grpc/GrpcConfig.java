71dfb0ec4143f666f1a470cab653eb156be10e24
JunHoPark <pjh374968@gmail.com>
2019-02-22 02:15:44 +0900

    refactor: 설정 변경
    maxlifetime 수정, gRPC keep alive 1분 설정

--- src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
+++ src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
@@ -5,6 +5,8 @@ import io.grpc.ManagedChannelBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author junho.park
  */
@@ -13,6 +15,7 @@ public class GrpcConfig {
     @Bean
     public ManagedChannel setChannel() {
         return ManagedChannelBuilder.forAddress("49.236.136.197", 6565)
+                .keepAliveTime(60, TimeUnit.SECONDS)
                 .usePlaintext().build();
     }
 }
