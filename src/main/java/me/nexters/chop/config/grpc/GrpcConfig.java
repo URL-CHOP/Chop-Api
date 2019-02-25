d5f3353e7b5718da50a2fd4c3a9a8649939fde8e
JunHoPark <pjh374968@gmail.com>
2019-02-26 00:35:41 +0900

    refactor: 설정 테스트

--- src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
+++ src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
@@ -5,8 +5,6 @@ import io.grpc.ManagedChannelBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author junho.park
  */
@@ -15,9 +13,7 @@ public class GrpcConfig {
     @Bean
     public ManagedChannel setChannel() {
         return ManagedChannelBuilder.forAddress("49.236.136.197", 6565)
-                .keepAliveWithoutCalls(true)
+                .keepAliveWithoutCalls(false)
                 .usePlaintext().build();
-
-
     }
 }
