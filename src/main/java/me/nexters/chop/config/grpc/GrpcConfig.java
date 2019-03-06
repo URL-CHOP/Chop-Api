174d6d8f23dfcc6f22f73d2543dafcb4f4fbd145
JunHoPark <pjh374968@gmail.com>
2019-03-07 00:04:25 +0900

    refactor: gRPC keep alive time 수정
    https://github.com/grpc/grpc-java/issues/5400 예상 해결책에 따름

--- src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
+++ src/main/java/me/nexters/chop/config/grpc/GrpcConfig.java
@@ -5,6 +5,8 @@ import io.grpc.ManagedChannelBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author junho.park
  */
@@ -13,7 +15,8 @@ public class GrpcConfig {
     @Bean
     public ManagedChannel setChannel() {
         return ManagedChannelBuilder.forAddress("49.236.136.197", 6565)
-                .keepAliveWithoutCalls(false)
-                .usePlaintext().build();
+                .usePlaintext()
+                .keepAliveTime(5, TimeUnit.MINUTES)
+                .build();
     }
 }
